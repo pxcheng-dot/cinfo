89904f6467e38512222810d5a699b3033a36c2eb
ReinhartLab <sarenaphilps@me.com>
2026-04-28 10:53:04 -0400

    Initial Commit

---
+++ cinfo/cinfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cinfoApp.swift
+//  cinfo
+//
+//  Created by PhilpSarena on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct cinfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
